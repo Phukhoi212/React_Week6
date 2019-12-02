9038bc6b50fe89d6876f3fc0e9112bdb2d4029fa
Minh_Sang <sang.nguyen@terralogic.com>
2019-12-02 15:46:56 +0700

    up code

---
+++ plan-week6/src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TableUsers from '../../components/Table/TableUsers'
+
+class Home extends React.Component{
+  render(){
+    return(
+      <div>
+        <TableUsers />
+      </div>
+    );
+  }
+}
+
+export default Home;
